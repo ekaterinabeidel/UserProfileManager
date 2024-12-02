bfe1047c2d2d90757f1903b255a1495311456212
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-12-02 23:54:45 +0100

    add upload avatar to interface user profile service

--- src/main/kotlin/ekaterinabeidel/userprofilemanager/service/UserProfileService.kt
+++ src/main/kotlin/ekaterinabeidel/userprofilemanager/service/UserProfileService.kt
@@ -3,9 +3,11 @@ package ekaterinabeidel.userprofilemanager.service
 import ekaterinabeidel.userprofilemanager.dto.UpdateUserProfileDto
 import ekaterinabeidel.userprofilemanager.dto.UserProfileDto
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
 
 @Service
 interface UserProfileService {
     fun getUserProfile(userId: Long): UserProfileDto
     fun updateUserProfile(userId: Long, updateUserProfileDto: UpdateUserProfileDto): UserProfileDto
+    fun uploadAvatar(userId: Long, file: MultipartFile)
 }
