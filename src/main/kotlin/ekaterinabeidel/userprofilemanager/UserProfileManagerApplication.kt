9d298444c60441399976fd1e6d8ce05abb342757
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-12-02 11:38:28 +0100

    init project

---
+++ src/main/kotlin/ekaterinabeidel/userprofilemanager/UserProfileManagerApplication.kt
@@ -0,0 +1,11 @@
+package ekaterinabeidel.userprofilemanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserProfileManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserProfileManagerApplication>(*args)
+}
---
+++ src/test/kotlin/ekaterinabeidel/userprofilemanager/UserProfileManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ekaterinabeidel.userprofilemanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserProfileManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
