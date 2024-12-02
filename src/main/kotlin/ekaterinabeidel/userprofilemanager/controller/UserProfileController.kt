740b6ee7be2b019bf74fd99188a0e1e57ece5783
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-12-02 23:53:11 +0100

    add upload avatar to user profile controller

--- src/main/kotlin/ekaterinabeidel/userprofilemanager/controller/UserProfileController.kt
+++ src/main/kotlin/ekaterinabeidel/userprofilemanager/controller/UserProfileController.kt
@@ -3,6 +3,7 @@ package ekaterinabeidel.userprofilemanager.controller
 import ekaterinabeidel.userprofilemanager.dto.UpdateUserProfileDto
 import ekaterinabeidel.userprofilemanager.dto.UserProfileDto
 import ekaterinabeidel.userprofilemanager.service.UserProfileService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/api")
-class UserProfileController(private val userProfileService: UserProfileService) {
+class UserProfileController(
+    private val userProfileService: UserProfileService,
+    @Value("\${file.upload-dir}") private val uploadDir: String) {
 
     @GetMapping("/profile")
     fun getUserProfile(@RequestParam userId: Long): ResponseEntity<UserProfileDto> {
@@ -26,5 +30,13 @@ class UserProfileController(private val userProfileService: UserProfileService) 
     ): ResponseEntity<UserProfileDto> {
         val updatedProfile = userProfileService.updateUserProfile(userId, updateUserProfileDto)
         return ResponseEntity.ok(updatedProfile)
+    }
+
+    @PutMapping("/upload-avatar")
+    fun uploadAvatar(@RequestParam userId: Long,
+                     @RequestParam("file") file: MultipartFile
+    ): ResponseEntity<Map<String, String>>{
+        val avatarUrl = userProfileService.uploadAvatar(userId, file)
+        return ResponseEntity.ok(mapOf("URL" to avatarUrl))
     }
 }
